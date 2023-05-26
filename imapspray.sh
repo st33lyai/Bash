eab111613ccd8dd3407943df8e9b091ce8609849
st33lyai <133703532+st33lyai@users.noreply.github.com>
2023-05-26 18:19:57 -0400

    Update imapspray.sh

--- imapspray.sh
+++ imapspray.sh
@@ -1,8 +1,11 @@
 #! /bin/bash
 
 # This script is used to spray imaps accounts with passwords from a wordlist.
+# If anything but the username:password combination return, (I.E. '* LIST') the credentials above the value should be valid.
+# Inspired by this page from Hacktricks: https://book.hacktricks.xyz/network-services-pentesting/pentesting-imap
 
-# Check for correct number of arguments, exit if 3 aren't provided
+# Check for correct number of arguments, exit if 3 aren't provided.
+
 if [ $# -ne 3 ]; then
         echo "Usage: imapspray.sh <IP> <userlist> <wordlist>"
         exit 1
